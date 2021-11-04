d0f01e91bd006ebf596c582b0ccd250f529cab8d
Ben Dowsett <bendowsett@gmail.com>
2021-11-04 13:24:36 +0000

    viewing hit points

--- app.rb
+++ app.rb
@@ -16,11 +16,14 @@ end
 post '/names' do
     session[:player_1_name] = params[:player_1_name]
     session[:player_2_name] = params[:player_2_name]
-    erb :play
+    redirect '/play'
   end
 
-
-
+get '/play' do 
+  @player_1_name = session[:player_1_name]
+  @player_2_name = session[:player_2_name]
+  erb :play
+end 
 
 
 run! if app_file == $0
--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -10,4 +10,13 @@ feature 'Enter names' do
       expect(page).to have_content 'Charlotte vs. Mittens'
     end
   end
-    
+  
+  feature 'View hit Points' do 
+     scenario 'see player 2 hit points' do 
+      visit('/')
+      fill_in :player_1_name, with: 'Charlotte'
+      fill_in :player_2_name, with: 'Mittens'
+      click_button 'Submit'
+      expect(page).to have_content 'Mittens: 60hp'
+     end 
+  end 
