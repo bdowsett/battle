a34ef2a827797a83e96a0b8ddddd59e4fd4f76ee
Ben Dowsett <bendowsett@gmail.com>
2021-11-04 13:29:31 +0000

    test helpers

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,11 +1,9 @@
 feature 'Enter names' do
     scenario 'submitting names' do
       visit('/')
-      fill_in :player_1_name, with: 'Charlotte'
-      fill_in :player_2_name, with: 'Mittens'
-      click_button 'Submit'
+      sign_in_and_play
       
-      save_and_open_page # will save the web page and open the browser to display it
+      save_and_open_page 
       
       expect(page).to have_content 'Charlotte vs. Mittens'
     end
@@ -14,9 +12,7 @@ feature 'Enter names' do
   feature 'View hit Points' do 
      scenario 'see player 2 hit points' do 
       visit('/')
-      fill_in :player_1_name, with: 'Charlotte'
-      fill_in :player_2_name, with: 'Mittens'
-      click_button 'Submit'
+      sign_in_and_play
       expect(page).to have_content 'Mittens: 60hp'
      end 
   end 
