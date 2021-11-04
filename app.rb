76cc80d20670f5389682ddd99a363b8f193f7035
Ryan Grimes <ryanpaulgrimes@gmail.com>
2021-11-04 17:15:42 +0100

    Add skinny controllers

--- app.rb
+++ app.rb
@@ -13,20 +13,21 @@ get '/' do
 end 
 
 post '/names' do
-  $player_1 = Player.new(params[:player_1_name])
-  $player_2 = Player.new(params[:player_2_name])
+  $game = Game.new(Player.new(params[:player_1_name]), Player.new(params[:player_2_name]))
+  # $player_1 = Player.new(params[:player_1_name])
+  # $player_2 = Player.new(params[:player_2_name])
     redirect '/play'
   end
 
 get '/play' do 
-  @player_1_name = $player_1.name
-  @player_2_name = $player_2.name
+  @player_1_name = $game.player_1.name
+  @player_2_name = $game.player_2.name
   erb :play
 end 
 
 get '/attack' do
-  @player_1_name = $player_1.name
-  @player_2_name = $player_2.name
+  @player_1_name = $game.player_1.name
+  @player_2_name = $game.player_2.name
   erb :attack
 end
 
--- lib/game.rb
+++ lib/game.rb
@@ -1,19 +1,19 @@
 require_relative 'player'
 
 class Game
-    def initialize(player_1, player_2)
-      @players = [player_1, player_2]
-    end 
+  def initialize(player_1, player_2)
+    @players = [player_1, player_2]
+  end 
 
-    def player_1
-        @players[0]
-    end 
+  def player_1
+      @players[0]
+  end 
 
-    def player_2
-      @players[1]
-    end 
+  def player_2
+    @players[1]
+  end 
 
-    def attack(player_name)
-        player_name.reduce_health
-    end 
+  def attack(player_name)
+      player_name.reduce_health
+  end 
 end 
