4cb9252cbfd8b3c9078cd45d00560c6a82dcb4a3
Ben Dowsett <bendowsett@gmail.com>
2021-11-04 13:38:56 +0000

    attack

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -16,3 +16,11 @@ feature 'Enter names' do
       expect(page).to have_content 'Mittens: 60hp'
      end 
   end 
+
+  feature 'Attack' do
+    scenario 'attack Player 2' do
+      sign_in_and_play
+      click_link 'Attack'
+      expect(page).to have_content 'Charlotte attacked Mittens'
+    end 
+  end 
